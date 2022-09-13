9ee9ee89f35ecef7df1a0d64b0626b7b3eeba310
HaraT0323 <alra9si916323@gmail.com>
2022-09-13 22:56:47 +0900

    イベントハンドラの追加

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -11,7 +12,34 @@ func main() {
 		io.WriteString(w, "Hello, world!\n")
 	}
 
+	postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Article...\n")
+	}
+
+	articleListHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Article List...\n")
+	}
+
+	articleDetailHandler := func(w http.ResponseWriter, req *http.Request) {
+		articleId := 1
+		resString := fmt.Sprintf("Article No.%d\n", articleId)
+		io.WriteString(w, resString)
+	}
+
+	postNiceHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Nice...\n")
+	}
+
+	postCommentHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Article Comment...\n")
+	}
+
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/article", postArticleHandler)
+	http.HandleFunc("/article/list", articleListHandler)
+	http.HandleFunc("/article/1", articleDetailHandler)
+	http.HandleFunc("/article/nice", postNiceHandler)
+	http.HandleFunc("/comment", postCommentHandler)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
