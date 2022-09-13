7c7f10d67934be6198476f5a276e9e71c2a1eef9
HaraT0323 <alra9si916323@gmail.com>
2022-09-14 00:18:56 +0900

    投稿一覧取得リンクエストへのクエリパラの追加

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -18,7 +18,25 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List...\n")
+	queryMap := req.URL.Query()
+
+	var page int
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		// pageパラメータに複数値が入ってる場合は初めのものを採用する
+		page, err = strconv.Atoi(p[0])
+		// クエリパラが数字以外であればエラーを出力
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		// クエリパラが付与されていない場合は、page=1が付与されているとみなす
+		page = 1
+	}
+
+	resString := fmt.Sprintf("Article List (page %d)\n", page)
+	io.WriteString(w, resString)
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
