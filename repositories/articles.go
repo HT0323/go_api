c640a2370f5688b0cdce8faa36740275e597abf9
HaraT0323 <alra9si916323@gmail.com>
2022-09-17 00:15:02 +0900

    記事のいいね数を1追加する関数を追加

--- repositories/articles.go
+++ repositories/articles.go
@@ -88,3 +88,43 @@ func SelectArticleDetail(db *sql.DB, articleID int) (models.Article, error) {
 
 	return article, nil
 }
+
+// 記事のいいね数を1増やす
+func UpdateNiceNum(db *sql.DB, articleID int) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	const sqlGetNice = `
+		select nice
+		from articles
+		where article_id = ?;
+	`
+
+	row := tx.QueryRow(sqlGetNice, articleID)
+	if err := row.Err(); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	var niceNum int
+	err = row.Scan(&niceNum)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	const sqlUpdateNice = `update articles set nice = ? article_id = ?;`
+	_, err = tx.Exec(sqlUpdateNice, niceNum+1, articleID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
