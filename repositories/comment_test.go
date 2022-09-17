170d7e1e517d0bb96b0c0359ee72c2e051c19b6e
HaraT0323 <alra9si916323@gmail.com>
2022-09-17 22:09:06 +0900

    InsertComment関数のテストを追加

--- repositories/comment_test.go
+++ repositories/comment_test.go
@@ -1,8 +1,10 @@
 package repositories_test
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/HT0323/go_api/models"
 	"github.com/HT0323/go_api/repositories"
 )
 
@@ -18,4 +20,30 @@ func TestSelectCommentList(t *testing.T) {
 			t.Errorf("want comment of articleID %d but got ID %d\n", articleID, comment.ArticleID)
 		}
 	}
+}
+
+func TestInsertComment(t *testing.T) {
+	comment := models.Comment{
+		ArticleID: 1,
+		Message:   "CommentInsertTest",
+	}
+
+	expectedCommentID := 6
+	newComment, err := repositories.InsertComment(testDB, comment)
+	if err != nil {
+		t.Error(err)
+	}
+	if newComment.CommentID != expectedCommentID {
+		t.Errorf("new comment id is expected %d but got %d\n", expectedCommentID, newComment.CommentID)
+	}
+
+	t.Cleanup(func() {
+		const sqlStr = `
+			delete from comments
+			where message = ?
+		`
+		got, err := testDB.Exec(sqlStr, comment.Message)
+		fmt.Println(got)
+		fmt.Println(err)
+	})
 }
