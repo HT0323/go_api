62a0ea7ff7304017e5673c808009fb19b673ac3e
HaraT0323 <alra9si916323@gmail.com>
2022-09-18 00:12:58 +0900

    PosCommentHandlerのサービス層の実装

---
+++ services/comment_service.go
@@ -0,0 +1,21 @@
+package services
+
+import (
+	"github.com/HT0323/go_api/models"
+	"github.com/HT0323/go_api/repositories"
+)
+
+func PostCommentService(comment models.Comment) (models.Comment, error) {
+	db, err := connectDB()
+	if err != nil {
+		return models.Comment{}, err
+	}
+	defer db.Close()
+
+	newComment, err := repositories.InsertComment(db, comment)
+	if err != nil {
+		return models.Comment{}, err
+	}
+
+	return newComment, nil
+}
