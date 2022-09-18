390a33b205ff33e785d9f7e6598675f5551f74c6
HaraT0323 <alra9si916323@gmail.com>
2022-09-18 17:47:10 +0900

    ArticleDetailHandlerのテストの追加

--- controllers/article_controller_test.go
+++ controllers/article_controller_test.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
 func TestArticleListHandler(t *testing.T) {
@@ -24,6 +26,35 @@ func TestArticleListHandler(t *testing.T) {
 
 			res := httptest.NewRecorder()
 			aCon.ArticleListHandler(res, req)
+
+			if res.Code != tt.resultCode {
+				t.Errorf("unexpected StatusCode: want %d but %d\n", tt.resultCode, res.Code)
+			}
+		})
+	}
+}
+
+func TestArticleDetailHandler(t *testing.T) {
+	var tests = []struct {
+		name       string
+		articleID  string
+		resultCode int
+	}{
+		{name: "number pathparam", articleID: "1", resultCode: http.StatusOK},
+		{name: "alphabet pathparam", articleID: "aaa", resultCode: http.StatusNotFound},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			url := fmt.Sprintf("http://localhost:8080/article/%s", tt.articleID)
+			req := httptest.NewRequest(http.MethodGet, url, nil)
+
+			res := httptest.NewRecorder()
+
+			r := mux.NewRouter()
+			r.HandleFunc("/article/{id:[0-9]+}",
+				aCon.ArticleDetailHandler).Methods(http.MethodGet)
+			r.ServeHTTP(res, req)
 
 			if res.Code != tt.resultCode {
 				t.Errorf("unexpected StatusCode: want %d but %d\n", tt.resultCode, res.Code)
