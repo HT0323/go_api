cdf6805484194a83fe74f3095de7200ce14bb3ed
HaraT0323 <alra9si916323@gmail.com>
2022-09-18 22:35:26 +0900

    レスポンス情報をログに出力

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -5,10 +5,29 @@ import (
 	"net/http"
 )
 
+type resLoggingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+func NewResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
+	return &resLoggingWriter{ResponseWriter: w, code: http.StatusOK}
+}
+
+func (rsw *resLoggingWriter) WriteHeader(code int) {
+	rsw.code = code
+	rsw.ResponseWriter.WriteHeader(code)
+}
+
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		// リクエスト情報をロギング
 		log.Println(req.RequestURI, req.Method)
-		next.ServeHTTP(w, req)
+
+		rlw := NewResLoggingWriter(w)
+
+		next.ServeHTTP(rlw, req)
+
+		log.Println("res: ", rlw.code)
 	})
 }
